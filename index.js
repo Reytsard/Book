f825ff3662a06caa737b427f48843e5ace3c5346
Ronald <ronaldreyjr061201@gmail.com>
2023-03-17 18:33:14 +0800

    Added new book button and form

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, has ${this.pages}, has not been read`;
 }
 
-
 function addBookToLibrary(book){
     library = library.push(book);
 }
+
+const addForm = '<fieldset>\n<div class="titleForBook">\n<label for="title">Title:</label>\n<input type="text" name="title" id="title" placeholder="Title">\n</div>\n<div class="authorForBook">\n<label for="author">Author:</label>\n<input type="text" name="author" id="author" placeholder="Author">\n</div>\n<div class="pagesForBook">\n<label for="pages">Pages:</label>\n<input type="text" name="pages" id="pages" placeholder="pages">\n</div>\n<div class="isReadForBook">\n<label for="isRead">Read?</label>\n<input type="radio" name="isRead" id="isRead" value="true">yes\n<input type="radio" name="isRead" id="isRead" value="false">no\n</div>\n<button type="submit" id="submitBookForm">Add Book</button>\n</fieldset>';
+
+const addBookButton = document.querySelector('#addBook');
+const addBookForm = document.querySelector('.addBookForm');
+addBookButton.addEventListener('click',() => {
+    addBookButton.toggleAttribute('disabled');
+    addBookForm.innerHTML = addForm;
+    const submitBookForm = addBookForm.querySelector('#submitBookForm');
+        submitBookForm.addEventListener('click', () => {
+        addBookButton.toggleAttribute('disabled');
+        addBookForm.innerHTML="";
+    });
+})
