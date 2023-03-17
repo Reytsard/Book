224b6d6db2ed6d5cb2d9124da7b961e579ef3660
Ronald <ronaldreyjr061201@gmail.com>
2023-03-17 23:13:56 +0800

    made button so that it makes a div when submitted

--- index.js
+++ index.js
@@ -12,21 +12,58 @@ Book.prototype.info = function(){
     }
     return `${this.title} by ${this.author}, has ${this.pages}, has not been read`;
 }
+let bookShelf = document.querySelector('.bookshelf');
 
-function addBookToLibrary(book){
-    library = library.push(book);
-}
 
-const addForm = '<fieldset>\n<div class="titleForBook">\n<label for="title">Title:</label>\n<input type="text" name="title" id="title" placeholder="Title">\n</div>\n<div class="authorForBook">\n<label for="author">Author:</label>\n<input type="text" name="author" id="author" placeholder="Author">\n</div>\n<div class="pagesForBook">\n<label for="pages">Pages:</label>\n<input type="text" name="pages" id="pages" placeholder="pages">\n</div>\n<div class="isReadForBook">\n<label for="isRead">Read?</label>\n<input type="radio" name="isRead" id="isRead" value="true">yes\n<input type="radio" name="isRead" id="isRead" value="false">no\n</div>\n<button type="submit" id="submitBookForm">Add Book</button>\n</fieldset>';
 
-const addBookButton = document.querySelector('#addBook');
-const addBookForm = document.querySelector('.addBookForm');
-addBookButton.addEventListener('click',() => {
-    addBookButton.toggleAttribute('disabled');
-    addBookForm.innerHTML = addForm;
-    const submitBookForm = addBookForm.querySelector('#submitBookForm');
-        submitBookForm.addEventListener('click', () => {
-        addBookButton.toggleAttribute('disabled');
-        addBookForm.innerHTML="";
-    });
+// bookDiv.appendChild(titleDiv);
+// bookDiv.appendChild(authorDiv);
+// bookDiv.appendChild(pagesDiv);
+// bookDiv.appendChild(isReadDiv);
+
+
+document.querySelector('#submitBookForm').addEventListener('click',() => {
+    // const newBook = new Book(document.querySelector('#title').value.toString(),
+    // document.querySelector('#author').value.toString(),
+    // Number.parseInt(document.querySelector('#pages').value),
+    // document.querySelector('#isRead').value);
+    // console.log(newBook);
+    let bookDiv = document.createElement('div');
+    let titleDiv = document.createAttribute('div');
+    let authorDiv = document.createAttribute('div');
+    let pagesDiv = document.createAttribute('div');
+    let isReadDiv = document.createAttribute('div');
+
+    bookDiv.classList.toggle('book');
+    bookDiv.appendChild(titleDiv);
+    bookDiv.appendChild(authorDiv);
+    bookDiv.appendChild(pagesDiv);
+    bookDiv.appendChild(isReadDiv);
+    bookShelf.appendChild(bookDiv);
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// addBookButton.addEventListener('click',() => {
+//     addBookButton.toggleAttribute('disabled');
+//     const submitBookForm = addBookForm.querySelector('#submitBookForm');
+//         submitBookForm.addEventListener('click', () => {
+//         addBookButton.toggleAttribute('disabled');
+//         const inputs = addBookForm.querySelectorAll('input');
+//         addBookForm.innerHTML="";
+//     });
+// })
