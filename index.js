e29007b3141dd3a6f87cba9d102df7f86908d7c4
Ronald <ronaldreyjr061201@gmail.com>
2023-03-18 22:38:21 +0800

    fixed ui

--- index.js
+++ index.js
@@ -8,66 +8,48 @@ function Book(title, author, pages, isRead){
 }
 Book.prototype.info = function(){
     if(this.isRead == "true"){
-        return `<div class="title">Title: ${this.title}</div>\n<div class="author">Author: ${this.author}</div>\n<div class="pages">Pages: ${this.pages} pages</div>\n<div class="isRead">Has been read</div>\n`;
+        return `<div class="title">Title: ${this.title}</div>\n<div class="author">Author: ${this.author}</div>\n<div class="pages">Pages: ${this.pages} pages</div>\n<div class="isRead"><button class="readButton" type="button" value="1">Has been Read</button></div>\n`;
     }
-    return `<div id="title">Title: ${this.title}</div>\n<div id="author">Author: ${this.author}</div>\n<div id="pages">Pages: ${this.pages} pages</div>\n<div id="isRead">Has not been read</div>\n`;
+    return `<div id="title">Title: ${this.title}</div>\n<div id="author">Author: ${this.author}</div>\n<div id="pages">Pages: ${this.pages} pages</div>\n<div id="isRead"><button class="readButton" type="button" value="0">Has not been Read</button></div>\n`;
 }
 function addToLibrary(book){
     library.push(book);
 }
-let bookShelf = document.querySelector('.bookshelf');
-
-
-
-// bookDiv.appendChild(titleDiv);
-// bookDiv.appendChild(authorDiv);
-// bookDiv.appendChild(pagesDiv);
-// bookDiv.appendChild(isReadDiv);
-
-
-document.querySelector('#submitBookForm').addEventListener('click',() => {
-    //gets the values from the input box;
-    let title = document.querySelector('#title').value.toString();
-    let author = document.querySelector('#author').value.toString();
-    let pages = Number.parseInt(document.querySelector('#pages').value);
-    let isRead = document.querySelector('#isRead').value;
-    //creates a new Object
-    let newBook = new Book(title,author,pages,isRead);
-    addToLibrary(newBook);
+function createAddBookForm(){
+    let addFormDiv = document.querySelector('.add-Book-Form');
+    addFormDiv.innerHTML = addBookForm;
+    document.querySelector('#submitBookForm').addEventListener('click',() => {
+        //gets the values from the input box;
+        let title = document.querySelector('#title').value.toString();
+        let author = document.querySelector('#author').value.toString();
+        let pages = Number.parseInt(document.querySelector('#pages').value);
+        let isRead = document.querySelector('#isRead').value;
     
+        //creates a new Object
+        let newBook = new Book(title,author,pages,isRead);
+        addToLibrary(newBook);
+    
+        //adds a card for the book
+        let bookDiv = document.createElement('div');
+        bookShelf.appendChild(bookDiv);
+        bookDiv.classList.toggle('book');
+        bookDiv.innerHTML = newBook.info();
+        let readButton = bookDiv.querySelector("button");
+        readButton.addEventListener('click',() => {
+            if(readButton.value == '1'){
+                readButton.textContent = "Has not been Read";
+                readButton.setAttribute('value','0'); 
+            }else{
+                readButton.innerHTML = 'Has been Read';
+                readButton.setAttribute('value','1');
+            }
+        });
+        addFormDiv.innerHTML = '';
+    });
+}
 
-    console.log(library);
-
-    //adds a card for the book
-    let bookDiv = document.createElement('div');
-    bookShelf.appendChild(bookDiv);
-    bookDiv.classList.toggle('book');
-    bookDiv.innerHTML = newBook.info();
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// addBookButton.addEventListener('click',() => {
-//     addBookButton.toggleAttribute('disabled');
-//     const submitBookForm = addBookForm.querySelector('#submitBookForm');
-//         submitBookForm.addEventListener('click', () => {
-//         addBookButton.toggleAttribute('disabled');
-//         const inputs = addBookForm.querySelectorAll('input');
-//         addBookForm.innerHTML="";
-//     });
-// })
+//main
+const libraryDiv = document.querySelector('.library');
+const bookShelf = document.querySelector('.bookshelf');
+const addBookForm = '<fieldset>\n<div class="titleForBook">\n<label for="title">Title:</label>\n<input type="text" name="title" id="title" placeholder="Title">\n</div>\n<div class="authorForBook">\n<label for="author">Author:</label>\n<input type="text" name="author" id="author" placeholder="Author">\n</div>\n<div class="pagesForBook">\n<label for="pages">Pages:</label>\n<input type="number" name="pages" id="pages" placeholder="pages">\n</div>\n<div class="isReadForBook">\n<label for="isRead">Read?</label>\n<select name="isRead" id="isRead">\n<option value="true" selected>yes</option>\n<option value="false">no</option>\n</select>\n<button type="submit" id="submitBookForm">Add Book</button></fieldset>\n';
+document.querySelector('#addBook').addEventListener('click', createAddBookForm);
